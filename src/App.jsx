49a0cacce87a4a71b7c89a1fd497989775487d0e
AmirAliAmin <aliaminamir77@gmail.com>
2025-02-19 11:22:17 +0500

    mini Counter Project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  const setValue = ()=>{
+    if (counter >= 20) {
+      setCounter(20)
+    }
+    else{
+      counter = counter+1;
+      setCounter(counter);
+      // console.log("clicked" , counter);
+    }
+  }
+  const removeValue = ()=>{
+    if (counter<=0) {
+      setCounter(0)
+    }
+    else{
+      setCounter(counter - 1);
+  
+    }
+  }
+  
+
+  return (
+    <>
+      <h1>Amir Ali Amin</h1>
+      <h2>Counter is :{counter} </h2>
+      <br />
+      <button style={{backgroundColor:"black", color:"white", marginRight:"20px"}} onClick={setValue} >Add Value</button>
+      <button style={{backgroundColor:"black", color:"white"}} onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
